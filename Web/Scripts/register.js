74f55010308963f61ed1abbbb577e5a6831969b9
Petr Povazhniy <ess_service@hotmail.com>
2016-12-13 16:53:01 +0300

    register

---
+++ Web/Scripts/register.js
@@ -0,0 +1,43 @@
+﻿var Register = function (steps) {
+    var totalSteps;
+    var model = {
+        total:ko.observable(),
+        currentStep:ko.observable(1),
+        back: function() {
+            if (model.currentStep() > 1) {
+                model.currentStep(model.currentStep() - 1);
+            }
+        },
+        next: function() {
+            if (model.currentStep() < totalSteps) {
+                model.currentStep(model.currentStep() + 1);
+            }
+        },
+        done: function() {
+            console.log("User press Done");
+            model.currentStep(1);
+        },
+        isLastStep: ko.pureComputed(function () {
+            return model.currentStep() === totalSteps;
+        },this),
+        isFirstStep:ko.pureComputed(function() {
+            return model.currentStep() === 1;
+        }, this),
+        user: {
+            userName: ko.observable(),
+
+        }
+    };
+
+    var init = function () {
+        totalSteps = steps;
+        model.total(steps);
+        ko.applyBindings(model, document.getElementById("wizard"));
+    };
+
+    $(init);
+
+    return {
+        wizard: wizard
+    };
+}(3);
